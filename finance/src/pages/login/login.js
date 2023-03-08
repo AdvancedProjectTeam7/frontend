2046a26caf0df5c61c88e10a2b413770dc3132fc
abdelhadicodi <aboude.arab12@gmail.com>
2023-03-08 23:24:32 +0200

    login page not finish

--- finance/src/pages/login/login.js
+++ finance/src/pages/login/login.js
@@ -1,25 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./login.css";
+import { useState } from "react";
+import logo from "./images/logo-finance2.png";
 
-class Login extends Component {
-  state = {
-    email: "",
-    password: "",
-  };
-  handleEmailChange = (event) => {
-    this.setState({
-      email: event.target.value,
-    });
-  };
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  handlePasswordChange = (event) => {
-    this.setState({
-      password: event.target.value,
-    });
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
-  handleLogin = () => {
-    const { email, password } = this.state;
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
     fetch("http://localhost:8000/api/auth/login", {
       method: "POST",
       headers: {
@@ -29,10 +25,11 @@ class Login extends Component {
     })
       .then((res) => res.json())
       .then((data) => {
-        if (data) {
+        if (data.token) {
           localStorage.setItem("loggedInAdminId", data.id);
           localStorage.setItem("token", data.token);
           localStorage.setItem("email", data.email);
+          console.log(email);
         } else {
           alert("Invalid credentials");
         }
@@ -41,44 +38,56 @@ class Login extends Component {
         console.error("Error checking credentials:", error);
       });
   };
-  render() {
-    const { email, password } = this.state;
 
-    return (
-      <div className="login-div">
-        <h1 className="login">Sign In</h1>
-        <div className="login-form">
-          <div className="textInputWrapper">
+  return (
+    <>
+    <div className="background-login"></div>
+    <div className="container">
+      <div className="left-side">
+        <img className="logo1" src={logo} width={500} height={320} />
+        <h2 className="logo-title">Finance Management</h2>
+      </div>
+      <div className="right-side">
+        <form className="login-form">
+          <h1 className="login-title">Login</h1>
+
+          <div className="email-wrapper">
             <input
-              className="textInput"
-              placeholder="Insert your email"
               type="email"
+              className="input-email"
               id="email"
               value={email}
-              onChange={this.handleEmailChange}
+              onChange={handleEmailChange}
               required
             />
+            <label className="email" htmlFor="email">
+              Email
+            </label>
           </div>
-          <div className="textInputWrapper1">
+          <div className="pass-wrapper">
             <input
-              className="textInput1"
-              placeholder="Insert your password"
               type="password"
-              id="password"
+              className="input-pass"
+              id="pass"
               value={password}
-              onChange={this.handlePasswordChange}
+              onChange={handlePasswordChange}
               required
             />
+            <label className="pass" htmlFor="pass">
+              Password
+            </label>
           </div>
-          <div className="btn">
-            <button className="btn-1" onClick={this.handleLogin}>
-              Login
-            </button>
-          </div>
-        </div>
+
+          <button className="btn-login" onClick={handleLogin}>
+            {" "}
+            Button
+            <span></span>
+          </button>
+        </form>
       </div>
-    );
-  }
+    </div>
+    </>
+  );
 }
 
 export default Login;
