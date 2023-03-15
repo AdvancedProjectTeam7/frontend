8b7aaeea65a8a07d2d928a8ccebf2582da505165
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-03-15 14:26:30 +0200

    ee

--- finance/src/allroutes/index.js
+++ finance/src/allroutes/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "../pages/login/login.js";
 import DashBoard from "../pages/sidebar/dashboard.jsx";
+import Transactions from "../pages/transactions/transactions.jsx";
 // import PrivateRoute from "../Review/PrivateRoute.jsx";
 // import PublicRoutes from "../Review/PublicRoute.jsx";
 
@@ -10,6 +11,7 @@ class AllRoutes extends Component {
         return (
             <BrowserRouter>
                 <Routes>
+                    <Route path="/transactions" element={<Transactions />} />
                     <Route path="/dashboard" element={<DashBoard />} />
                     <Route path="/" element={<Login />} />
                 </Routes>
---
+++ finance/src/pages/transactions/transactions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashBoard from "../sidebar/dashboard";
+import "./transactions.css";
+
+const Transactions = () => {
+    return (
+        <>
+            <DashBoard />
+        </>
+    );
+};
+export default Transactions;
