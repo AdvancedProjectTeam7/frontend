54f366fc7b9eaebe37c970ab223008005a8eeece
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-03-20 11:57:46 +0200

    test2

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 // import {BrowserRouter} from "react-router-dom";
-import AllRoutes  from "./allroutes/index.js"
+import AllRoutes from "./allroutes/index.js";
 // import Dashboard from "./pages/sidebar/dashboard.js"
 
-
 function App() {
-  return (
-    <div className='main-page' style={{display: "flex"}}> 
-      <AllRoutes />
-    </div>  
-  )
+    return (
+        <>
+            <AllRoutes />
+        </>
+    );
 }
 
-export default App
+export default App;
--- finance/src/allroutes/index.js
+++ finance/src/allroutes/index.js
@@ -3,27 +3,34 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "../pages/login/login.js";
 import DashBoard from "../pages/sidebar/dashboard.jsx";
 
-import Admin from "../pages/admins-panel/admin.js"
-import CreateAdmin from "../pages/admins-panel/create-admin.jsx"
+import Admin from "../pages/admins-panel/admin.js";
+import CreateAdmin from "../pages/admins-panel/create-admin.jsx";
 
 import PrivateRoute from "../Review/PrivateRoute.jsx";
 import PublicRoutes from "../Review/PublicRoute.jsx";
 import User from "../pages/admins-panel/edit-admin.js";
+import TestPage from "../pages/testPage/testPage.jsx";
+import Transactions from "../pages/transactions/transactions.jsx";
 
 class AllRoutes extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Routes>
+                <div className="main-table">
+                    <DashBoard />
+                    <Routes>
+                        <Route path="/admin" element={<Admin />} />
+                        <Route path="/add" element={<CreateAdmin />} />
+                        <Route path="/edit" element={<User />} />
 
-                    <Route path="/" element={<DashBoard />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="/add" element={<CreateAdmin />} />
-                    <Route path="/edit" element={<User />} />
-
-
-                </Routes>
+                        <Route
+                            path="/transactions"
+                            element={<Transactions />}
+                        />
+                        <Route path="/dashboard" element={<TestPage />} />
+                        <Route path="/" element={<Login />} />
+                    </Routes>
+                </div>
             </BrowserRouter>
         );
     }
