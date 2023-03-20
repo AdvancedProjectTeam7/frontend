0f408fde4917cd9feb32a451d2b513a99c7e62dd
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-03-20 12:30:01 +0200

    working

--- finance/src/allroutes/index.js
+++ finance/src/allroutes/index.js
@@ -2,13 +2,8 @@ import React, { Component } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "../pages/login/login.js";
 import DashBoard from "../pages/sidebar/dashboard.jsx";
-
-import Admin from "../pages/admins-panel/admin.js";
-import CreateAdmin from "../pages/admins-panel/create-admin.jsx";
-
 import PrivateRoute from "../Review/PrivateRoute.jsx";
 import PublicRoutes from "../Review/PublicRoute.jsx";
-import User from "../pages/admins-panel/edit-admin.js";
 import TestPage from "../pages/testPage/testPage.jsx";
 import Transactions from "../pages/transactions/transactions.jsx";
 
@@ -19,10 +14,6 @@ class AllRoutes extends Component {
                 <div className="main-table">
                     <DashBoard />
                     <Routes>
-                        <Route path="/admin" element={<Admin />} />
-                        <Route path="/add" element={<CreateAdmin />} />
-                        <Route path="/edit" element={<User />} />
-
                         <Route
                             path="/transactions"
                             element={<Transactions />}
