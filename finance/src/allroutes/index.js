bd01e1ef7d11fc6d6ab4844c492685f1cdc1e1a0
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-03-20 11:38:44 +0200

    test

--- finance/src/allroutes/index.js
+++ finance/src/allroutes/index.js
@@ -4,8 +4,12 @@ import Login from "../pages/login/login.js";
 import DashBoard from "../pages/sidebar/dashboard.jsx";
 import TestPage from "../pages/testPage/testPage.jsx";
 import Transactions from "../pages/transactions/transactions.jsx";
+import CreateAdmin from "../pages/admins-panel/create-admin.jsx"
+import Admin from "../pages/admins-panel/admin.js"
+
 // import PrivateRoute from "../Review/PrivateRoute.jsx";
 // import PublicRoutes from "../Review/PublicRoute.jsx";
+import User from "../pages/admins-panel/edit-admin.js";
 
 class AllRoutes extends Component {
     render() {
@@ -17,6 +21,9 @@ class AllRoutes extends Component {
                     <Route path="/transactions" element={<Transactions />} />
                     <Route path="/dashboard" element={<TestPage />} />
                     <Route path="/" element={<Login />} />
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/add" element={<CreateAdmin />} />
+                    <Route path="/edit" element={<User />} />
                 </Routes>
             </div>
             </BrowserRouter>
