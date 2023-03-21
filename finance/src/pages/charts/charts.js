b91a2206879ac01161e0c02aaa30d5cc07e25ddd
Yahyabakkar <yahyabakkar022@gmail.com>
2023-03-21 21:07:09 +0200

    noth

--- finance/src/pages/charts/charts.js
+++ finance/src/pages/charts/charts.js
@@ -2,16 +2,13 @@ import React from "react";
 import "./charts.css";
 import LineGraph from "./linegraph";
 import PieGraph from "./piechart";
-import axios from "axios";
 const Charts = () => {
-  const URL = "http://localhost:8000/";
-
   return (
     <>
-        <div className="wrapper">
-          <LineGraph />
-          <PieGraph />
-        </div>
+      <div className="wrapper">
+        <LineGraph />
+        <PieGraph />
+      </div>
     </>
   );
 };
