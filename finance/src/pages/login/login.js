d3839760af92e618293d793e0b7a2662c9da51ba
fatinaelomar3 <fatinaelomar3@gmail.com>
2023-03-22 09:03:29 +0200

    hhh

--- finance/src/pages/login/login.js
+++ finance/src/pages/login/login.js
@@ -1,35 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./login.css";
 import logo from "./images/logo-finance2.png";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [inputsFilled, setInputsFilled] = useState({
-    email: false,
-    password: false,
-  });
-  const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [inputsFilled, setInputsFilled] = useState({
+        email: false,
+        password: false,
+    });
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-    setInputsFilled((prev) => ({
-      ...prev,
-      email: Boolean(event.target.value.trim()),
-    }));
-  };
+    const navigate = useNavigate();
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-    setInputsFilled((prev) => ({
-      ...prev,
-      password: Boolean(event.target.value.trim()),
-    }));
-  };
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+        setInputsFilled((prev) => ({
+            ...prev,
+            email: Boolean(event.target.value.trim()),
+        }));
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        setInputsFilled((prev) => ({
+            ...prev,
+            password: Boolean(event.target.value.trim()),
+        }));
+    };
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -40,20 +41,16 @@ function Login() {
                 { email, password }
             );
             const data = response.data;
-            //   console.log("Server response:", data);
             if (data && data.access_token) {
                 localStorage.setItem("loggedInAdminId", data.id || "");
                 localStorage.setItem("token", data.access_token || "");
                 localStorage.setItem("email", data.email || "");
-
-                // use `await` to wait for the toast to complete
                 await toast.success("Success Login !", {
                     position: toast.POSITION.TOP_CENTER,
                 });
-
                 setTimeout(() => {
                     navigate("/dashboard");
-                }, 2000); // wait for 2 seconds before navigating
+                }, 2000);
             } else {
                 throw new Error("Invalid email or password");
             }
@@ -65,62 +62,72 @@ function Login() {
         }
     };
 
-  return (
-    <>
-      <div className="container">
-        <div className="left-side">
-          <img className="logo1" src={logo} alt="logo" />
-          <h2 className="logo-title">Finance Management</h2>
-        </div>
-        <div className="right-side">
-          <form className="login-form">
-            <h1 className="login-title">Login</h1>
+    return (
+        <>
+            <div className="container">
+                <div className="left-side">
+                    <img className="logo1" src={logo} alt="logo" />
+                    <h2 className="logo-title">Masroofak</h2>
+                </div>
+                <div className="right-side">
+                    <form className="login-form">
+                        <h1 className="login-title">Login</h1>
 
-            <div className={`email-wrapper ${email ? "has-value" : ""}`}>
-              <input
-                type="email"
-                className="input-email"
-                id="email"
-                value={email}
-                onChange={handleEmailChange}
-                required
-              />
-              <label
-                className={`email ${inputsFilled.email ? "input-filled" : ""}`}
-                htmlFor="email"
-              >
-                Email
-              </label>
-            </div>
-            <div className={`pass-wrapper ${password ? "has-value" : ""}`}>
-              <input
-                type="password"
-                className="input-pass"
-                id="pass"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-              />
-              <label
-                className={`pass ${
-                  inputsFilled.password ? "input-filled" : ""
-                }`}
-                htmlFor="pass"
-              >
-                Password
-              </label>
-            </div>
+                        <div
+                            className={`email-wrapper ${
+                                email ? "has-value" : ""
+                            }`}
+                        >
+                            <input
+                                type="email"
+                                className="input-email"
+                                id="email"
+                                value={email}
+                                onChange={handleEmailChange}
+                                required
+                            />
+                            <label
+                                className={`email ${
+                                    inputsFilled.email ? "input-filled" : ""
+                                }`}
+                                htmlFor="email"
+                            >
+                                Email
+                            </label>
+                        </div>
+                        <div
+                            className={`pass-wrapper ${
+                                password ? "has-value" : ""
+                            }`}
+                        >
+                            <input
+                                type="password"
+                                className="input-pass"
+                                id="pass"
+                                value={password}
+                                onChange={handlePasswordChange}
+                                required
+                            />
+                            <label
+                                className={`pass ${
+                                    inputsFilled.password ? "input-filled" : ""
+                                }`}
+                                htmlFor="pass"
+                            >
+                                Password
+                            </label>
+                        </div>
 
-            <button className="btn-login" onClick={handleLogin}>
-              {" "}
-              Login
-              <span></span>
-            </button>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+                        <button className="btn-login" onClick={handleLogin}>
+                            {" "}
+                            Login
+                            <span></span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Login;
